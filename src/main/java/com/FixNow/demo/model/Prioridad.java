2844f8c100ca09fe2254979104a6af271e8f8954
Maty-5 <matiasjeldres2005@gmail.com>
2026-04-09 00:56:21 -0400

    Prueba Evaluacion 1 de Matias Jeldres

---
+++ src/main/java/com/FixNow/demo/model/Prioridad.java
@@ -0,0 +1,12 @@
+package com.FixNow.demo.model;
+
+/**
+ * Define el nivel de urgencia o importancia
+ * de una incidencia en el sistema (BAJA, MEDIA, ALTA Y CRITICA).
+ */
+public enum Prioridad {
+    BAJA,
+    MEDIA,
+    ALTA,
+    CRITICA
+}
